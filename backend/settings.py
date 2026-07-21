10f89cf339b6def19f72fc99585201aac6dbbc85
prince iheanacho <iheanachop2000@gmail.com>
2026-07-21 16:08:36 -0700

    second commit

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY''django-insecure-+g46^^36#&)fexxd*2)+og-ca290y@&yfslc)6gb%6sb4e%uw8')
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-+g46^^36#&)fexxd*2)+og-ca290y@&yfslc)6gb%6sb4e%uw8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG','False') == 'True'
